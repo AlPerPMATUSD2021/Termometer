0fd8ce345a213562336c9167883849e4fefdeff4
unknown <leokumara48@gmail.com>
2021-10-02 16:53:43 +0700

    tugas termometer tanggal 2 Oktober 2021 kelompok 1

--- termometer.js
+++ termometer.js
@@ -1,5 +1,35 @@
-function termometer(k) {
+function KtoC(k) {
     return c = k - 273.15
 }
 
-console.log(termometer(4))
+console.log(KtoC(4));
+
+function CtoK(c) {
+    return k = c + 273.15
+}
+
+console.log(CtoK(7));
+
+function FtoC(f) {
+    return c = ((f - 32) * 5/9) 
+}
+
+console.log(FtoC(41));
+
+function CtoF(c) {
+    return f = ((c * 9/5) + 32)
+}
+
+console.log(CtoF(5))
+
+function KtoF(k) {
+    return f = ((k - 273.15) * 9/5 + 32)
+}
+
+console.log(KtoF(273.15))
+
+function FtoK(f) {
+    return k = ((f - 32) * 5/9 + 273.15)
+}
+
+console.log(FtoK(32))
