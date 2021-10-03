b5891583af75f80d07c8263617c697c8da4f68ab
unknown <theolaola12@gmail.com>
2021-10-03 22:56:21 +0700

    Tugas Termometer Klmpk 4

---
+++ termometer.js
@@ -0,0 +1,32 @@
+//Agnes Dwi Risaningsih 211414001
+function KtoC(k) {
+    return c = k-273
+}
+console.log(KtoC(23));
+
+function CtoK(c) {
+    return k = c+273
+}
+console.log(CtoK(27));
+
+//Christina Theola Putri Wardoyo 211414044
+function FtoK(F) {
+    return K = 5/9*(F-32)+273.15
+}
+console.log(FtoK(10));
+
+function KtoF(K) {
+    return F = 9/5*(K-273.15)+32
+}
+console.log(KtoF(10));
+
+//Daviana Widya Maurora Putri 211414053
+function FtoC(F) {
+    return C = (F-32)*5/9
+}
+console.log(FtoC(5));
+
+function CtoF(C) {
+    return F = (C*9/5)+32
+}
+console.log(CtoF(10));
