58aa0cf7886ac2126af7c385144f818943f8cee2
User <greimonn08@gmail.com>
2021-10-04 21:20:07 +0800

    Tugas Termometer

---
+++ Termometer.js
@@ -0,0 +1,65 @@
+function FtoC (F) {
+    return c = (F-32)*5/9
+}
+console.log (FtoC(5));
+
+function CtoF (C){
+    return F = (c*9/5)+32
+}
+console.log (CtoF(5));
+
+function KtoC (K){
+    return C = K-273.15
+}
+console.log (KtoC(5));
+
+function CtoK (C){
+    return K = C + 273.15
+}
+console.log (CtoK(5));
+
+function RtoC (R){
+    return C = (R-491.67)*5/9
+}
+console.log (RtoC(5));
+
+function CtoR (C){
+    return R = 9/5+491.67
+}
+console.log (CtoR(5));
+
+
+function RtoF (R){
+    return F = R-456.67
+}
+console.log(RtoF(5));
+
+
+function FtoR (F){
+    return R = F+459.67
+}
+console.log(FtoR(5));
+
+
+function RtoK (R){
+    return K = R*5/9
+}
+console.log(RtoK(5));
+
+
+function KtoR (K){
+    return R = K
+}
+console.log(KtoR(5));
+
+
+function FtoK (F){
+    return K = (F-32)*5/9+273.15
+}
+console.log(FtoK(5));
+
+
+function KtoF (K){
+    return F = (K-273.15)*9/5+32
+}
+console.log(KtoF(5));
