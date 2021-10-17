513b5e49e7bd0c6e3bad8c01e9265b0fb95d7974
unknown <brigittaananta@gmail.com>
2021-10-17 12:03:31 +0700

    tugas termometer 2

--- termometer.js
+++ termometer.js
@@ -1,5 +1,45 @@
-function termometer (k) {
+function KtoC (k) {
     return c = k - 273.15
 }
 
-console.log (termometer(4));
+console.log (KtoC(4));
+
+
+
+function KtoF (k) {
+    return f = (k - 273.15) * 9/5 + 32
+}
+
+console.log (KtoF(4));
+
+
+
+function CtoK (c) {
+    return k = c + 273.15
+}
+
+console.log (CtoK(4));
+
+
+
+function CtoF (c) {
+    return f = c * 9/5 + 32
+}
+
+console.log (CtoF(4));
+
+
+
+function FtoC (f) {
+    return c = (f - 32) * 9/5
+}
+
+console.log (FtoC(4))
+
+
+
+function FtoK (f) {
+    return k = (f - 32) * 9/5 + 273.15
+}
+
+console.log (FtoK(4))
